ba76e9d6768ef55b0ee88812dc2d87b07145f7c4
shreeyashah09 <shreeya.shah16@gmail.com>
2023-08-06 15:30:01 -0400

    adding changes in the app in background

--- app.py
+++ app.py
@@ -4,9 +4,26 @@ from PIL import Image
 import numpy as np
 import os
 
-st.write("""
-Sign Language Classification Model
-""")
+# Custom CSS to style the app
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #f9f9f9;
+    }
+    .stButton>button {
+        background-color: #feca57;
+        color: #ffffff;
+        font-weight: bold;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Custom header
+st.image("https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhWi5-x1EvY3iq6DTWLttZ7-Ym0fLpXEuSR5Ctu_jPxHyI8PL7axqyt6iCajI7QuKgVqX15gaiLP_RJr-A6GNINxsRv3noZkyDBaT2pZsoW2BNdxppejifHQRZwdCExYlZLIzIPYyJ_PDJ9z_-HqGse1T_gW8f4sFW4NF5T2mI_UOjT7HCrMXMr7Vor/w1200-h630-p-k-no-nu/GDS_ASL_FingerspellingCompetition_Banners_Metacard.png", use_column_width=True)
+st.title(":wave: Sign Language Classification Model :muscle:")
 
 # Load the pre-trained model
 def load_model():
@@ -38,7 +55,21 @@ uploaded_image = file_selector()
 if uploaded_image is not None:
     img = Image.open(uploaded_image)
     st.image(img, caption="Your Image", use_column_width=True)
-    st.write("Classifying...")
+    st.markdown("### :rocket: Classifying Image...")
     label_class, label_confidence = predict(uploaded_image)
-    st.write('The image is %d with %.2f%% probability' % (label_class, label_confidence * 100))
+
+    # Emojis representing the predicted class
+    emojis = [":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:"]
+    predicted_emoji = emojis[label_class]
+
+    # Display the prediction with a colorful banner
+    st.markdown(
+        """
+        <div style="background-color: #feca57; padding: 10px; border-radius: 5px;">
+            <h3 style="color: #ffffff;">Prediction Result:</h3>
+            <p style="font-size: 24px; color: #ffffff;">The image is {} with {:.2f}% probability</p>
+        </div>
+        """.format(predicted_emoji, label_confidence * 100),
+        unsafe_allow_html=True
+    )
 
